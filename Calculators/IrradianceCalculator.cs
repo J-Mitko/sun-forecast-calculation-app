60559132c71df16f3fe06cdd62afa0a9c99398a9
Dimitar Pelev <dimitarpelev@gmail.com>
2024-02-12 11:48:39 +0200

    fix irradiance calculation formula

--- Calculators/IrradianceCalculator.cs
+++ Calculators/IrradianceCalculator.cs
@@ -5,13 +5,14 @@ namespace sun_forecast_calculation_app.Calculator
 {
     class IrradianceCalculator : IIrradianceCalculator
     {
+        //Direct irradiance incident at the top of the atmosphere.
         private const double dni_extra = 1367.0;
         private SunIrradiationData sunIrradiance = new SunIrradiationData();
 
         public SunIrradiationData calculateIrradians(double sunZenith, double airmass, double transmittens)
         {
-            sunIrradiance.dni = dni_extra * transmittens * airmass;
-            sunIrradiance.dhi = 0.3 * (1.0 - transmittens * airmass) * dni_extra * sunZenith;
+            sunIrradiance.dni = dni_extra * Math.Pow(transmittens, airmass);
+            sunIrradiance.dhi = 0.3 * (1.0 - Math.Pow(transmittens,airmass)) * dni_extra * sunZenith;
             sunIrradiance.ghi = sunIrradiance.dhi + sunIrradiance.dni * sunZenith;
 
             return sunIrradiance;
